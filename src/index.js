7ea108b0b494eb5c2aeb1596adeee61409889f7f
bterricita <113158912+bterricita@users.noreply.github.com>
2023-06-20 14:57:08 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+let paris = {
+  name: "Paris",
+  country: "France",
+  language: "French",
+  temperature: 18,
+  capitalCity: true
+};
+//Given an object called paris, log its temperature
+console.log(paris.temperature);
+
+//Create an object called sydney similar to paris and log its temperature (use fake data)
+let sydney = {
+  name: "Sydney",
+  country: "Australia",
+  language: "English",
+  temperature: 80,
+  capitalCity: false
+};
+console.log(sydney.temperature);
+//Create an array with 3 different Australian city names only and log each name
+let cities = ["Canberra", "Melbourne", "Darwin"];
+console.log(cities[0]);
+console.log(cities[1]);
+console.log(cities[2]);
